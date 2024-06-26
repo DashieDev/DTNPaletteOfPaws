1216010afdea1221bf7a66edfd3f140078ec2b70
DashieDev <lamtinh.asian@gmail.com>
2024-06-26 19:33:16 +0700

    DTN Support Dog Variant Icon and guiColor

--- src/main/java/dtnpaletteofpaws/dtn_support/variant/DTNSupportDogVariants.java
+++ src/main/java/dtnpaletteofpaws/dtn_support/variant/DTNSupportDogVariants.java
@@ -14,10 +14,10 @@ public class DTNSupportDogVariants {
     
     public static final DeferredRegister<DogVariant> DOG_VARIANT = DeferredRegister.create(DoggyRegistries.Keys.DOG_VARIANT, Constants.MOD_ID);
 
-    public static final Supplier<DogVariant> CAPPUCCINO = register("cappuccino");
-    public static final Supplier<DogVariant> ESPRESSO = register("espresso");
-    public static final Supplier<DogVariant> LATTE = register("latte");
-    public static final Supplier<DogVariant> MOCHA = register("mocha");
+    public static final Supplier<DogVariant> CAPPUCCINO = register("cappuccino", 0xffae7437);
+    public static final Supplier<DogVariant> ESPRESSO = register("espresso", 0xff280d03);
+    public static final Supplier<DogVariant> LATTE = register("latte", 0xff9b7f66);
+    public static final Supplier<DogVariant> MOCHA = register("mocha", 0xff5d2b00);
     public static final Supplier<DogVariant> WITHERED_SOUL = register("withered_soul", WitheredSoulDogVariant::new);
 
     private static Supplier<DogVariant> register(String name, Function<String, DogVariant> variant_creator) {
@@ -25,13 +25,15 @@ public class DTNSupportDogVariants {
         return DOG_VARIANT.register(name, () -> captured_variant);
     }
 
-    private static Supplier<DogVariant> register(String name) {
-        return register(name, p -> {});
+    private static Supplier<DogVariant> register(String name, int gui_color) {
+        return register(name, p -> {}, gui_color);
     }
 
-    private static Supplier<DogVariant> register(String name, Consumer<DogVariant.Props> props_consumer) {
+    private static Supplier<DogVariant> register(String name, Consumer<DogVariant.Props> props_consumer, int gui_color) {
         var props = DogVariant.props(Util.getResource(name))
-            .customTexture(Util.modifyPath(Util.getResource(name), x -> "textures/entity/dtnwolf/variants/wolf_" + x));
+            .customTexture(Util.modifyPath(Util.getResource(name), x -> "textures/entity/dtnwolf/variants/wolf_" + x))
+            .customTranslation(Constants.MOD_ID + ".variant." + name)
+            .guiColor(gui_color);
         props_consumer.accept(props);
         final var captured_variant = new DogVariant(props);
         return DOG_VARIANT.register(name, () -> captured_variant);
--- src/main/java/dtnpaletteofpaws/dtn_support/variant/WitheredSoulDogVariant.java
+++ src/main/java/dtnpaletteofpaws/dtn_support/variant/WitheredSoulDogVariant.java
@@ -4,6 +4,7 @@ import doggytalents.api.impl.DogAlterationProps;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.inferface.IDogAlteration;
 import doggytalents.common.variant.DogVariant;
+import dtnpaletteofpaws.common.lib.Constants;
 import dtnpaletteofpaws.common.lib.Resources;
 import dtnpaletteofpaws.common.util.RandomUtil;
 import dtnpaletteofpaws.common.util.Util;
@@ -18,6 +19,8 @@ public class WitheredSoulDogVariant extends DogVariant implements IDogAlteration
             .glowingOverlay(
                 Resources.getDTNWolfTexture("variants/wolf_withered_soul_overlay")
             )
+            .customTranslation(Constants.MOD_ID + ".variant." + name)
+            .guiColor(0xff01a7ac)
         );
     }
 
