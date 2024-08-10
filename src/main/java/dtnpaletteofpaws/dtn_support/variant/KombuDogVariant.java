c3091ad2ff04dc6f9653bb3147c1cf32d74a2392
DashieDev <lamtinh.asian@gmail.com>
2024-08-11 00:41:16 +0700

    add kombu ability to swim underwater DTN

---
+++ src/main/java/dtnpaletteofpaws/dtn_support/variant/KombuDogVariant.java
@@ -0,0 +1,27 @@
+package dtnpaletteofpaws.dtn_support.variant;
+
+import doggytalents.api.impl.DogAlterationProps;
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.api.inferface.IDogAlteration;
+import doggytalents.common.variant.DogVariant;
+import dtnpaletteofpaws.common.lib.Constants;
+import dtnpaletteofpaws.common.util.Util;
+
+public class KombuDogVariant extends DogVariant implements IDogAlteration {
+
+    public KombuDogVariant(String name) {
+        super(
+            DogVariant.props(Util.getResource(name))
+            .customTexture(Util.modifyPath(Util.getResource(name), x -> "textures/entity/dtnwolf/variants/wolf_" + x))
+            .customTranslation(Constants.MOD_ID + ".variant." + name)
+            .guiColor(0xff36491d)
+        );
+    }
+
+    @Override
+    public void props(AbstractDog dog, DogAlterationProps props) {
+        props.setCanSwimUnderwater();
+        props.setCanBreatheUnderwater();
+        props.addBonusSwimSpeed(10);
+    }
+}
