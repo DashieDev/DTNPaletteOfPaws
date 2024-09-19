eecb4d57d9064cdcc487f7270d2c877bf8e5a77c
DashieDev <lamtinh.asian@gmail.com>
2024-09-19 15:56:15 +0700

    static spawn manager and change how biome is determined for wolf variant
    Variant is now determined by the original spawn target biome instead of
    wherever the dog is standing to prevent dog from spawning off-biome.

--- src/main/java/dtnpaletteofpaws/common/mixin/NaturalSpawnerMixin.java
+++ src/main/java/dtnpaletteofpaws/common/mixin/NaturalSpawnerMixin.java
@@ -5,18 +5,24 @@ import javax.annotation.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import dtnpaletteofpaws.ChopinLogger;
 import dtnpaletteofpaws.DTNEntityTypes;
 import dtnpaletteofpaws.common.spawn.DTNWolfSpawnPlacements;
 import dtnpaletteofpaws.common.spawn.DTNWolfVariantsFabricSpawn;
+import dtnpaletteofpaws.common.spawn.DTNWolfStaticSpawnManager;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.NaturalSpawner;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.biome.Biome;
 
 @Mixin(NaturalSpawner.class)
 public class NaturalSpawnerMixin {
@@ -30,6 +36,11 @@ public class NaturalSpawnerMixin {
             DTNEntityTypes.DTNWOLF.get(), level, x, z);
         
         info.setReturnValue(pos);
+    }
+
+    @Inject(method = "spawnMobsForChunkGeneration", at = @At(value = "HEAD"), cancellable = false)
+    private static void dtn_spawnMobsForChunkGeneration(ServerLevelAccessor level_accessor, Holder<Biome> biome, ChunkPos chunk_pos, RandomSource random, CallbackInfo info) {
+        DTNWolfStaticSpawnManager.get().onChunkGenerationMobSpawn(level_accessor, biome, chunk_pos, random);
     }
 
     //Fabric
