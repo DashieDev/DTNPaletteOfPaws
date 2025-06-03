7f77bb272cad180fc8288f7c5ae23e96ca754782
DashieDev <lamtinh.asian@gmail.com>
2025-06-03 23:30:08 +0700

    force DTNP Pack icon when detected the pack belongs to DTNP

--- src/main/java/dtnpaletteofpaws/common/lib/Resources.java
+++ src/main/java/dtnpaletteofpaws/common/lib/Resources.java
@@ -5,6 +5,8 @@ import net.minecraft.resources.ResourceLocation;
 
 public class Resources {
 
+    public static ResourceLocation DTNP_PACK_ICON = Util.getResource("dtnp_pack_icon.png");
+
     public static ResourceLocation getDTNWolfTexture(String name) {
         var prefix = "textures/entity/dtnwolf/";
         return Util.getResource(prefix + name + ".png");
