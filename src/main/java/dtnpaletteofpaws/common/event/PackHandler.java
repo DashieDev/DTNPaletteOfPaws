fa72135eb42a136324b7dc2026fb9b798b66d4e6
DashieDev <lamtinh.asian@gmail.com>
2025-06-03 23:30:08 +0700

    force DTNP Pack icon when detected the pack belongs to DTNP

--- src/main/java/dtnpaletteofpaws/common/event/PackHandler.java
+++ src/main/java/dtnpaletteofpaws/common/event/PackHandler.java
@@ -1,8 +1,14 @@
 package dtnpaletteofpaws.common.event;
 
+import java.util.Optional;
+
+import dtnpaletteofpaws.common.lib.Constants;
+import dtnpaletteofpaws.common.lib.Resources;
 import dtnpaletteofpaws.common.util.Util;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.repository.Pack.Position;
 import net.neoforged.neoforge.event.AddPackFindersEvent;
@@ -14,6 +20,14 @@ public class PackHandler {
             "data/dtnpaletteofpaws/datapacks/dtnp_wolf_spawn_rate_inc");
         event.addPackFinders(pack_location, PackType.SERVER_DATA, Component.literal("DTNP Wolf Spawn Increase"), 
             PackSource.FEATURE, false, Position.TOP);
+    }
+
+    public static Optional<ResourceLocation> onPackLoadIcon(Pack pack) {
+        var pack_id = pack.getId();
+        if (pack_id != null && pack_id.startsWith("mod/" + Constants.MOD_ID)) {
+            return Optional.of(Resources.DTNP_PACK_ICON);
+        }
+        return Optional.empty();
     }
 
 }
