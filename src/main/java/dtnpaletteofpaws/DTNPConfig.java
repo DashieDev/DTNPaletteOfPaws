5c3fa7b34f5a8fe388e52a74d48f73bfe2a295c0
DashieDev <lamtinh.asian@gmail.com>
2025-06-04 00:11:05 +0700

    remove unecessary dtnp config

--- src/main/java/dtnpaletteofpaws/DTNPConfig.java
+++ src/main/java/dtnpaletteofpaws/DTNPConfig.java
@@ -20,15 +20,8 @@ public class DTNPConfig {
 
     public static class ServerConfig {
 
-        public ForgeConfigSpec.BooleanValue DTNP_SPAWN_TOO_COMMON_FIX;
-
         public ServerConfig(ForgeConfigSpec.Builder builder) {
-            DTNP_SPAWN_TOO_COMMON_FIX = builder
-                .comment("As of now, some biomes doesn't have much spawn causing some DTNP wolf variants")
-                .comment("to be much more common than anticipated. This option makes DTNP spawns")
-                .comment("in those biomes have a chance to cancel when attempting to carrying out the spawn.")
-                .translation("dtnpaletteofpaws.config.fix_dtnp_spawn_too_common ")
-                .define("fix_dtnp_spawn_too_common", true);
+            
         }
 
         public static<T> T getConfigOrDefault(ConfigValue<T> config, T defaultVal) {
