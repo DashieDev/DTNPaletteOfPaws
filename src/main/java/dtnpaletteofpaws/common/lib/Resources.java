f9e4dbfd3deb9cd201c905c079052cfc31feaea9
DashieDev <lamtinh.asian@gmail.com>
2025-06-19 01:59:02 +0700

    make resourse entry final

--- src/main/java/dtnpaletteofpaws/common/lib/Resources.java
+++ src/main/java/dtnpaletteofpaws/common/lib/Resources.java
@@ -5,7 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class Resources {
 
-    public static ResourceLocation DTNP_PACK_ICON = Util.getResource("dtnp_pack_icon.png");
+    public static final ResourceLocation DTNP_PACK_ICON = Util.getResource("dtnp_pack_icon.png");
 
     public static ResourceLocation getDTNWolfTexture(String name) {
         var prefix = "textures/entity/dtnwolf/";
